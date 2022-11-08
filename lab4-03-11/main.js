2367a051e9500d0378fb663fe15f3be2eb513c97
mnosel00 <mateusz.nosel@microsoft.wsei.edu.pl>
2022-11-08 20:36:51 +0100

    x

--- lab4-03-11/main.js
+++ lab4-03-11/main.js
@@ -7,15 +7,74 @@ class Note {
     this.date = date;
   }
 }
+const tbody = document.querySelector("tbody");
+window.addEventListener("load", displayLocalStorage);
 
-function display() {
-  const table = document.querySelector("tbody");
-  const tr = document.createElement("tr");
-  table.appendChild(tr);
+let id;
+let key;
+
+function displayLocalStorage() {
+  const allLocalStorageKeys = Object.keys(localStorage);
+
+  allLocalStorageKeys.forEach((key, id) => {
+    id++;
+    const noteObject = JSON.parse(localStorage.getItem(key));
+
+    const tr = document.createElement("tr");
+    tr.setAttribute("id", `${id}`);
+    tbody.appendChild(tr);
+
+    Object.values(noteObject).forEach((element) => {
+      const th = document.createElement("th");
+      th.innerHTML = element;
+      tr.appendChild(th);
+    });
+
+    const th = document.createElement("th");
+
+    const removeButton = document.createElement("button");
+    removeButton.setAttribute("class", "removeButton");
+    removeButton.innerHTML = "Remove";
+    th.appendChild(removeButton);
+
+    const editButton = document.createElement("button");
+    editButton.setAttribute("class", "editButton");
+    editButton.innerHTML = "Edit";
+    th.appendChild(editButton);
+
+    tr.appendChild(th);
+  });
 }
 
-const button = document.querySelector("#pass");
-button.addEventListener("click", () => {
+function displayFreshlyCreated(key) {
+  const noteObject = JSON.parse(localStorage.getItem(key));
+
+  const tr = document.createElement("tr");
+  tr.setAttribute("id", `${localStorage.length}`);
+  tbody.appendChild(tr);
+
+  Object.values(noteObject).forEach((element) => {
+    const th = document.createElement("th");
+    th.innerHTML = element;
+    tr.appendChild(th);
+  });
+
+  const th = document.createElement("th");
+
+  const removeButton = document.createElement("button");
+  removeButton.setAttribute("class", "removeButton");
+  removeButton.innerHTML = "Remove";
+  th.appendChild(removeButton);
+
+  const editButton = document.createElement("button");
+  editButton.setAttribute("class", "editButton");
+  editButton.innerHTML = "Edit";
+  th.appendChild(editButton);
+
+  tr.appendChild(th);
+}
+
+function createNoteObject() {
   const title = document.querySelector("#title");
   const content = document.querySelector("#content");
   const noteColor = document.querySelector("#noteColor");
@@ -30,6 +89,23 @@ button.addEventListener("click", () => {
     date.value
   );
 
-  localStorage.setItem(Date.now(), JSON.stringify(note));
-  display();
-});
+  const id = Date.now();
+  localStorage.setItem(id, JSON.stringify(note));
+  displayFreshlyCreated(id);
+}
+
+function removeNoteFromLocalStorage(key, id) {
+  // localStorage.removeItem(key);
+  // document.getElementById(`${id}`).remove();
+}
+
+function editExistingNote() {}
+
+const button = document.querySelector("#pass");
+button.addEventListener("click", createNoteObject);
+
+const removeButton = document.querySelector(".removeButton");
+removeButton.addEventListener("click", removeNoteFromLocalStorage);
+
+const editButton = document.querySelector(".editButton");
+editButton.addEventListener("click", editExistingNote);

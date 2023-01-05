e5c56b167be4e5fdc8225eff9b85ff3e835c2844
Mateusz Nosel <mateusz.nosel@microsoft.wsei.edu.pl>
2023-01-05 11:28:43 +0100

    api done

--- labX-15.12/main.js
+++ labX-15.12/main.js
@@ -5,6 +5,32 @@ const locationInput = document.querySelector("#location");
 
 const apiKey = "df2bf00f87e627058a8be4b13110d8b8";
 
+document.addEventListener("DOMContentLoaded", loadFromLocalStorage());
+
+function loadFromLocalStorage() {
+  let keys = Object.keys(localStorage);
+
+  keys.forEach((key) => {
+    const locationValue = localStorage.getItem(key);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${locationValue}&appid=${apiKey}`;
+
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Request failed");
+      })
+      .then((data) => {
+        createHTML(data, locationValue);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+}
+
 sumbitButton.addEventListener("click", () => {
   const locationValue = locationInput.value;
   locationInput.value = "";
@@ -19,42 +45,46 @@ sumbitButton.addEventListener("click", () => {
       throw new Error("Request failed");
     })
     .then((data) => {
-      let pogodaCard = document.createElement("div");
-      pogodaCard.className = "pogodaCard";
-
-      let icon = data.weather[0].icon;
-      let imgUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-
-      let img = document.createElement("img");
-      img.src = imgUrl;
-      img.alt = "123";
-
-      let h1 = document.createElement("h1");
-      h1.id = "pogodaCardLocation";
-      h1.innerHTML = `Location: ${locationValue}`;
-
-      let tempInFarenchait = data.main.temp;
-      let tempInCelcius = Math.round((tempInFarenchait - 32) * (5 / 9));
-
-      let pTemp = document.createElement("p");
-      pTemp.id = "temp";
-      pTemp.innerHTML = `Temperature: ${tempInCelcius}`;
-
-      let humidity = data.main.humidity;
-
-      let pHumidity = document.createElement("p");
-      pHumidity.id = "humidity";
-      pHumidity.innerHTML = `The humidity is ${humidity}%`;
-
-      pogodaCard.appendChild(img);
-      pogodaCard.appendChild(h1);
-      pogodaCard.appendChild(pTemp);
-      pogodaCard.appendChild(pHumidity);
-
-      document.body.appendChild(pogodaCard);
+      localStorage.setItem(Date.now(), locationValue);
+      createHTML(data, locationValue);
     })
     .catch((error) => {
       console.log(error);
     });
-  console.log(locationValue);
 });
+
+function createHTML(data, locationValue) {
+  let pogodaCard = document.createElement("div");
+  pogodaCard.className = "pogodaCard";
+
+  let icon = data.weather[0].icon;
+  let imgUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+  let img = document.createElement("img");
+  img.src = imgUrl;
+  img.alt = "123";
+
+  let h1 = document.createElement("h1");
+  h1.id = "pogodaCardLocation";
+  h1.innerHTML = `Location: ${locationValue}`;
+
+  let tempInFarenchait = data.main.temp;
+  let tempInCelcius = Math.round((tempInFarenchait - 32) * (5 / 9));
+
+  let pTemp = document.createElement("p");
+  pTemp.id = "temp";
+  pTemp.innerHTML = `Temperature: ${tempInCelcius}`;
+
+  let humidity = data.main.humidity;
+
+  let pHumidity = document.createElement("p");
+  pHumidity.id = "humidity";
+  pHumidity.innerHTML = `The humidity is ${humidity}%`;
+
+  pogodaCard.appendChild(img);
+  pogodaCard.appendChild(h1);
+  pogodaCard.appendChild(pTemp);
+  pogodaCard.appendChild(pHumidity);
+
+  document.body.appendChild(pogodaCard);
+}
